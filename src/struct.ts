ea3d7ae16159e693cb9ae90718d8a3147e41325d
Guzman Monne <guzmonne@hotmail.com>
2018-04-10 11:16:17 -0300

    fixed bug with superstruct

--- src/struct.ts
+++ src/struct.ts
@@ -1,5 +1,7 @@
 import { superstruct } from 'superstruct';
 
 export var struct = superstruct({
-  'string!': (x: any): boolean => typeof x === 'string' && x !== ''
+  types: {
+    'string!': (x: any): boolean => typeof x === 'string' && x !== ''
+  }
 });
